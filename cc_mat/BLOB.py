e28a88db74f3ebd87fafc819f6d7fb99d2089f94
math132d <majohha@gmail.com>
2018-11-09 13:20:12 +0100

    Implemented perimeter and circularity. optimised code?????

--- cc_mat/BLOB.py
+++ cc_mat/BLOB.py
@@ -5,6 +5,13 @@ class BLOB:
 
     def __init__(self):
         self.pixels = []
+        self.binary = None
+        self.area = None
+        self.centerOfMass = None
+        self.rect = None
+        self.compactness = None
+        self.circularity = None
+        self.perimeter = None
 
     def setPixels(self, pixels):
         self.pixels = pixels
@@ -12,46 +19,105 @@ class BLOB:
     def addPixel(self, pixel):
         self.pixels.append(pixel)
 
+    def getBinaryImg(self):
+        if(self.binary == None):
+            x, y, w, h = self.getRect()
+
+            self.binary = np.zeros((abs(w), abs(h)), np.bool_)
+
+            for p in self.pixels:
+                self.binary[p[0]-x][p[1]-y] = 1
+
+        return self.binary
+
+
     def getArea(self):
-        return len(self.pixels)
+        if(self.area == None):
+            self.area = len(self.pixels)
+
+        return self.area
 
     def getCenterOfMass(self):
+        if(self.centerOfMass == None):
+            xArr, yArr = np.hsplit(np.array(self.pixels), 2)
 
-        xArr, yArr = np.hsplit(np.array(self.pixels), 2)
+            xCom = (1/len(self.pixels)) * np.sum(xArr)
+            yCom = (1/len(self.pixels)) * np.sum(yArr)
 
-        xCom = (1/len(self.pixels)) * np.sum(xArr)
-        yCom = (1/len(self.pixels)) * np.sum(yArr)
+            self.centerOfMass = [int(xCom), int(yCom)]
 
-        return int(xCom), int(yCom) #Currently absolute center of mass, should be percentage of width/height
+        return self.centerOfMass[0], self.centerOfMass[1]
 
     def getRect(self):
+        if(self.rect == None):
 
-        x = 0
-        y = 0
-        w = 8000
-        h = 8000
+            x = 0
+            y = 0
+            w = 8000
+            h = 8000
 
-        for p in self.pixels:
-            if p[0] > x:
-                x = p[0]
-            if p[0] < w:
-                w = p[0]
-            if p[1] > y:
-                y = p[1]
-            if p[1] < h:
-                h = p[1]
+            for p in self.pixels:
+                if p[0] > x:
+                    x = p[0]
+                if p[0] < w:
+                    w = p[0]
+                if p[1] > y:
+                    y = p[1]
+                if p[1] < h:
+                    h = p[1]
 
-        return x, y, w-x, h-y
+            self.rect = [x, y, w-x, h-y]
+
+        return self.rect[0], self.rect[1], self.rect[2], self.rect[3]
 
     def getCompactness(self):
-        _, _, w, h = self.getRect()
-        area = self.getArea()
+        if(self.compactness == None):
+            _, _, w, h = self.getRect()
+            area = self.getArea()
 
-        return area / (w*h)
+            self.compactness = area / (w*h)
+
+        return self.compactness
+
+    def getPerimeter(self):
+        if(self.perimeter == None):
+            binImg = self.getBinaryImg()
+
+            counter = 0
+
+            for y in range(len(binImg)):
+                for x in range(len(binImg[y])):
+                    if(binImg[y][x] == 1):
+
+                        connectedSides = 0
+
+                        if(y-1 > 0):
+                            if(binImg[y-1][x] == 1):
+                                connectedSides += 1
+                        if(x-1 > 0):
+                            if(binImg[y][x-1] == 1):
+                                connectedSides += 1
+                        if(y+1 < len(binImg)):
+                            if(binImg[y+1][x] == 1):
+                                connectedSides += 1
+                        if(x+1 < len(binImg[y])):
+                            if(binImg[y][x+1] == 1):
+                                connectedSides += 1
+
+
+
+                        if(connectedSides < 4):
+                            counter += 1
+
+            self.perimeter = counter
+
+        return self.perimeter
 
     def getCircularity(self):
-        print("OI! getCircularity is not yet implemented!")
-        return 1 #Not yet implemented
+        if(self.circularity == None):
+            self.circularity = self.getPerimeter() / (2 * math.sqrt(math.pi * self.getArea()))
+
+        return self.circularity
 
 
 
@@ -67,4 +133,4 @@ def getBlobs(components):
         for x in range(len(components[y])):
             componentArray[components[y][x]-1].addPixel([x, y])
 
-    return componentArray
+    return componentArray[0:-1]
--- cc_mat/init.py
+++ cc_mat/init.py
@@ -9,7 +9,7 @@ def segment(image):
 
     return cv2.morphologyEx(thresh, cv2.MORPH_OPEN, np.ones((3, 3),np.uint8), iterations=1)
 
-image = cv2.imread('img2.png', 0)
+image = cv2.imread('mortenilna.png', 0)
 
 image = segment(image)
 
@@ -17,13 +17,20 @@ _, components = cv2.connectedComponents(image, connectivity=4)
 
 BLOBS = getBlobs(components)
 
-x, y, w, h = BLOBS[5].getRect()
-xCom, yCom = BLOBS[5].getCenterOfMass()
-
 image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
-cv2.rectangle(image, (x, y), (w+x, h+y), (0,255,0), 1)
-cv2.rectangle(image, (xCom-1, yCom-1), (xCom+1, yCom+1), (255,0,0), 1)
+for x in range(len(BLOBS)):
+    blob = BLOBS[x]
+
+    if(blob.getArea() > 60 and blob.getCompactness() < 0.75 and blob.getCompactness() > 0.5):
+
+        x, y, w, h = blob.getRect()
+        xCom, yCom = blob.getCenterOfMass()
+
+        print(blob.getCircularity())
+
+        cv2.rectangle(image, (x, y), (w+x, h+y), (0,255,0), 1)
+        cv2.rectangle(image, (xCom-1, yCom-1), (xCom+1, yCom+1), (255,0,0), 1)
 
 cv2.imshow("Image", image)
 cv2.waitKey(0)
